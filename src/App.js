d056ad499a8d76f36ba990c15d89da7cdaf7a3a6
Asif2535 <aiqbal517@gmail.com>
2021-03-03 19:09:38 +0600

    using areaChart

--- src/App.js
+++ src/App.js
@@ -1,76 +1,72 @@
 import './App.css';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import React, { PureComponent } from 'react';
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 function App() {
-  const lineChartData = [
+  const data = [
     {
-      name: "Page A",
+      name: 'Page A',
       uv: 4000,
       pv: 2400,
-      amt: 2400
+      amt: 2400,
     },
     {
-      name: "Page B",
+      name: 'Page B',
       uv: 3000,
       pv: 1398,
-      amt: 2210
+      amt: 2210,
     },
     {
-      name: "Page C",
+      name: 'Page C',
       uv: 2000,
       pv: 9800,
-      amt: 2290
+      amt: 2290,
     },
     {
-      name: "Page D",
+      name: 'Page D',
       uv: 2780,
       pv: 3908,
-      amt: 2000
+      amt: 2000,
     },
     {
-      name: "Page E",
+      name: 'Page E',
       uv: 1890,
       pv: 4800,
-      amt: 2181
+      amt: 2181,
     },
     {
-      name: "Page F",
+      name: 'Page F',
       uv: 2390,
       pv: 3800,
-      amt: 2500
+      amt: 2500,
     },
     {
-      name: "Page G",
+      name: 'Page G',
       uv: 3490,
       pv: 4300,
-      amt: 2100
-    }
+      amt: 2100,
+    },
   ];
   return (
     <div className="App.css">
-      <LineChart
-      width={500}
-      height={300}
-      data={lineChartData}
-      margin={{
-        top: 5,
-        right: 30,
-        left: 20,
-        bottom: 5
-      }}
-    >
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="name" />
-      <YAxis />
-      <Tooltip />
-      <Legend />
-      <Line
-        type="monotone"
-        dataKey="pv"
-        stroke="#8884d8"
-        activeDot={{ r: 8 }}
-      />
-      <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-    </LineChart>
+        <AreaChart
+          width={500}
+          height={400}
+          data={data}
+          margin={{
+            top: 10,
+            right: 30,
+            left: 0,
+            bottom: 0,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Area type="monotone" dataKey="uv" stackId="1" stroke="#8884d8" fill="#8884d8" />
+          <Area type="monotone" dataKey="pv" stackId="1" stroke="#82ca9d" fill="#82ca9d" />
+          <Area type="monotone" dataKey="amt" stackId="1" stroke="#ffc658" fill="#ffc658" />
+        </AreaChart>
     </div>
   );
 }
